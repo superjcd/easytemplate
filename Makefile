b8bebe2ab3baf0e24517bf2d3fb94e10dd7ffca5
jcd <929760274@qq.com>
2023-12-28 10:41:41 +0800

    version 0.1.0

---
+++ Makefile
@@ -0,0 +1,37 @@
+.DEFAULT_GOAL := all 
+
+# =============================================================================
+# Globals:
+ROOT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+OUTPUT_DIR  := $(ROOT_DIR)/build
+PLATFORMS ?=   linux_amd64 windows_amd64
+VERSION = 0.1.0
+COMMAND = easytemplate
+
+# =============================================================================
+
+.PHONY: all 
+all: tidy build 
+
+     
+.PHONY: tidy 
+tidy:
+	@go mod tidy
+.PHONY: build
+build:  $(foreach P,${PLATFORMS}, $(addprefix build., $(P)))
+
+.PHONY: build.%
+build.%:
+	$(eval OS:= $(word 1,$(subst _, ,$*)))
+	$(eval ARCH := $(word 2,$(subst _, ,$*)))  
+	$(if $(findstring windows, $(OS)), $(eval EXE_SUFFIX:=.exe), $(eval EXE_SUFFIX:=''))
+	@go env -w CGO_ENABLED=0  GOOS=$(OS) GOARCH=$(ARCH)
+	@echo "====>Build binary for ${COMMAND}, with OS: $(OS), ARCH:$(ARCH)"
+	@go build -o $(OUTPUT_DIR)/$(COMMAND)$(EXE_SUFFIX)  $(ROOT_DIR)
+
+
+
+.PHONY: clean
+clean:
+	$(if $(findstring Windows, $(OS)), $(shell rmdir /Q /S ${OUTPUT_DIR}), $(shell rm -rf ${OUTPUT_DIR}))
+	@echo "====>output directory  is removed sucessfully"
---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+)
+
+func fetchTemplates(templateSearch string) map[string]string {
+	allTemplates := make(map[string]string)
+	// add template: repository pair here
+	allTemplates["vue3-vite-ts"] = "https://github.com/superjcd/vue3-vite-ts-template"
+
+	if templateSearch != "" {
+		filteredTemplates := make(map[string]string)
+
+		for t, r := range allTemplates {
+			if strings.Contains(t, templateSearch) {
+				filteredTemplates[t] = r
+			}
+		}
+		return filteredTemplates
+
+	}
+
+	return allTemplates
+}
+
+func fetchTemplateOptions(templateName, templateRepo string) ([]string, error) {
+	var err error
+	cmd := exec.Command("git", "ls-remote", "--heads", templateRepo)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	optionsRaw := string(output)
+	optionsLines := strings.Split(optionsRaw, "\n")
+
+	templateOptions := make([]string, 0)
+
+	for _, line := range optionsLines {
+		if line != "" {
+			re := regexp.MustCompile(`refs/heads/(\w+)`)
+			fmt.Println(line)
+			matches := re.FindStringSubmatch(line)
+			fmt.Print(matches)
+			if matches != nil {
+				repoName := matches[1]
+				templateOptions = append(templateOptions, repoName)
+			}
+		}
+	}
+
+	if len(templateOptions) == 0 {
+		err = fmt.Errorf("oops, no options for template %s", templateName)
+	}
+
+	return templateOptions, err
+}
+
+func downLoadTempalate(repo, option, dirName string) {
+	var err error
+	cmd := exec.Command("git", "clone", "--branch", option, repo, dirName)
+	_, err = cmd.CombinedOutput()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var templateSearch string
+	var err error
+
+	inputArgs := os.Args
+	if len(inputArgs) == 2 {
+		templateSearch = inputArgs[1]
+	} else if len(inputArgs) > 2 {
+		panic("wrong input arguments, you can provide only one template name")
+	}
+
+	templates := fetchTemplates(templateSearch)
+	if len(templates) == 0 {
+		fmt.Printf("oops, no templates found")
+		return
+	}
+	possibleTemplates := make([]string, 0)
+
+	for t, _ := range templates {
+		possibleTemplates = append(possibleTemplates, t)
+	}
+
+	fmt.Println(possibleTemplates)
+
+	promptTemplate := promptui.Select{
+		Label: "select a template",
+		Items: possibleTemplates,
+	}
+
+	_, templateName, err := promptTemplate.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	templateRepo := templates[templateName]
+
+	options, err := fetchTemplateOptions(templateName, templateRepo)
+
+	if err != nil {
+		fmt.Printf("Get template options failed %v\n", err)
+		return
+	}
+
+	promptTemplateOptions := promptui.Select{
+		Label: "select a template",
+		Items: options,
+	}
+
+	_, templateOption, err := promptTemplateOptions.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	// fetch the template
+	var direName string
+	promptName := promptui.Prompt{
+		Label: "your project name?[myproject]",
+	}
+	direName, err = promptName.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if direName == "" {
+		direName = "myproject"
+	}
+	downLoadTempalate(templateRepo, templateOption, direName)
+
+}
